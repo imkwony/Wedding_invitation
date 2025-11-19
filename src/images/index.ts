d78ea506416488376cfd9b6123bc0d27d46cb664
Kwon <eckdrnjs@gmail.com>
2025-11-19 11:08:39 +0900

    photo change3

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,8 +1,8 @@
 import coverImage from "./cover.jpg"
-import image1 from "./image1.JPG"
-import image2 from "./image2.JPG"
-import image3 from "./image3.JPG"
-import image4 from "./image4.JPG"
+import image1 from "./image1.jpg"
+import image2 from "./image2.jpg"
+import image3 from "./image3.jpg"
+import image4 from "./image4.jpg"
 import image5 from "./image5.jpg"
 import image6 from "./image6.jpg"
 /*
