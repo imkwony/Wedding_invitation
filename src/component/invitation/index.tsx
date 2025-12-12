39fbf3f56db27d540fcf707e26dba1d0d8821f4b
Kwon <eckdrnjs@gmail.com>
2025-12-13 00:14:02 +0900

    invitation letter fix1

--- src/component/invitation/index.tsx
+++ src/component/invitation/index.tsx
@@ -25,19 +25,23 @@ export const Invitation = () => {
 
       <div className="break" />
 
-      <div className="content">싱그러운 봄 향기 가득한 날</div>
-      <div className="content">소중한 분들을 모시고</div>
-      <div className="content">사랑의 약속을 하려고 합니다.</div>
+      <div className="content">"그대 같은 사람 하나</div>
+      <div className="content">세상에 있어서</div>
+      <div className="content">세상이 좀 더 따스하고</div>
       <div className="break" />
-      <div className="content">햇살이 뜨거울 땐 가려주고,</div>
-      <div className="content">비가 오면 우산이 되어주는</div>
-      <div className="content">부부가 되겠습니다.</div>
+      <div className="content">서럽고도 벅찬 봄날이</div>
+      <div className="content">조금쯤 부드럽게</div>
+      <div className="content">흘러갑니다"</div>
       <div className="break" />
-      <div className="content">기쁜날 함께 하시어</div>
-      <div className="content">저희의 앞날을 축복해 주세요.</div>
+      <div className="content">- 나태주 &lt;봄날의 이유&gt; 중</div>
       <div className="break" />
-      <div className="content">창권이 장가간다</div>
-      <div className="content">오예</div>
+      <div className="break" />
+      <div className="content">세상을 좀 더 따스하게 만드는 존재를 찾았습니다.</div>
+      <div className="content">봄날을 부드럽게 흘러가게 하는 사람을 찾았습니다.</div>
+      <div className="content">4월의 봄날, 저희 두 사람의 시작을 함께해 주시면 감사하겠습니다.</div>
+      <div className="break" />
+      <div className="content">창권이 장가간다 오예~~</div>
+      <div className="content">사랑해 혜밍빠오♥</div>
       <div className="break" />
 
       <div className="name">
